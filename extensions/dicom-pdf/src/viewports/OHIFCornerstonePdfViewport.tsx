192b972cde4a2b2a54e223cdc27168916b691662
fuboki10 <abdelrahman.tarek1910@gmail.com>
2024-09-12 11:35:26 +0300

    FND-288 Add download pdf button

--- extensions/dicom-pdf/src/viewports/OHIFCornerstonePdfViewport.tsx
+++ extensions/dicom-pdf/src/viewports/OHIFCornerstonePdfViewport.tsx
@@ -21,7 +21,24 @@ function OHIFCornerstonePdfViewport({ displaySets }) {
   }, [pdfUrl]);
 
   return (
-    <div className="bg-primary-black h-full w-full text-white">
+    <div className="bg-primary-black flex h-full w-full flex-col text-white">
+      <div className="mx-auto">
+        <a
+          href={url}
+          className="inline-flex items-center rounded bg-gray-300 py-2 px-4 font-bold text-gray-800 hover:bg-gray-400"
+          role="button"
+          download
+        >
+          <svg
+            className="mr-2 h-4 w-4 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+          </svg>
+          <span>Download PDF</span>
+        </a>
+      </div>
       <object
         data={url}
         type="application/pdf"
